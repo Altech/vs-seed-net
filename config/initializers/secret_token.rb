71cc69b7b44e81114ef0112dbbff4b0a99592d85
Altech <takeno.sh@gmail.com>
2013-10-27 02:59:28 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VsSeed::Application.config.secret_key_base = '5a3f0dd9ea4b2f8ee52311a59ae47497ad75b80b9ffcb46112f947dca9d1d9855bf348c93403052b6fe6159ddbf40dc0abff27db3f8515624c3c9a35d955ff02'
