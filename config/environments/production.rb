ea48e53686f9f52571b8436e68a2eb4a093d29d6
Altech <takeno.sh@gmail.com>
2013-11-14 10:09:15 +0000

    use memcached as session store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ VsSeed::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.session_store = :mem_cache_store
 end
