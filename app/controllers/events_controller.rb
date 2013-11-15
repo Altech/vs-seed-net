3e01ea0c18bd2e816956b9c748db876c22f85e23
Altech <takeno.sh@gmail.com>
2013-11-15 11:57:41 +0000

    add a route to get a event page by date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,12 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:id])
+    if params[:id]
+      @event = Event.find(params[:id])
+    elsif params[:date]
+      t = Time.parse params[:date]
+      @event = Event.where(held_at: t..(t.to_date.next_day.to_time-1)).first
+    end
     @videos = @event.videos
     @game_center = @event.game_center
    end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ VsSeed::Application.routes.draw do
   post "login"  => 'log#auth'
   get  "logout" => 'log#out'
   resources :videos, only: %i[show]
+  match     'events/:date' => 'events#show', via: 'get', date: /\d\d-\d\d-\d\d/
   resources :events, only: %i[index show]
   resources :mechas, only: %i[index show]
   resources :comments, only: %i[create]
