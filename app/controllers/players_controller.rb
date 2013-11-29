b4bf8bb8ba41428de04ce5b9ed894229053b4065
Altech <takeno.sh@gmail.com>
2013-11-30 08:14:17 +0900

    add mecha ratio and winning percentage to player#show

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,6 +2,11 @@
 class PlayersController < ApplicationController
   def show
     @player = Player.find_by_name(params[:id]) or raise ActiveRecord::RecordNotFound
+    videos = @player.videos
+    if videos.size > 0
+      @mechas_ratio = [['機体','使用率']] + videos.map(&:mecha).select{|o| !o.nil?}.group_by(&:id).map{|id,mechas| [mechas.first.nickname, mechas.size]}
+      @winning_percentage = videos.select{|v| v.win_or_lose == true}.size * 100 / videos.size
+    end
   end
 
   def new
