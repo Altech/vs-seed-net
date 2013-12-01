7447f9bc6c698a611b5f49c213c08206891b6216
Altech <takeno.sh@gmail.com>
2013-12-01 19:59:08 +0900

    show comment icon in thumbnail if a comment exists

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
       comment = Comment.new comment_params
       comment.player_id = current_player.id
       comment.save
+      expire_fragment view_context.event_path(Video.find(params[:comment][:video_id]).event)
       render json: {result: 'success', comment: comment.attributes}
     else
       render json: {result: 'failure', message: 'コメントは1文字以上60文字以内です'}
