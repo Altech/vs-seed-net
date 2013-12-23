9add21f27d8ee87fa59d4d85f44da9831a94a359
Altech <takeno.sh@gmail.com>
2013-12-23 23:20:30 +0900

    impl automatic updating about information of continuous videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,18 +9,36 @@ class VideosController < ApplicationController
     @new_comment.video_id = @video.id
   end
 
-  # exclude validation of edit authority of the user for simplicity
+  # exclude validation of edit authority for simplicity
   def update
     video = Video.find(params[:id])
     video_param = params[:video]
     if video_param[:mecha_id].present?
-      video.update_attribute :mecha_id, video_param[:mecha_id]
+      update_attribute_of_continuous_videos(video, :mecha_id, video_param[:mecha_id])
       render json: {result: 'success', contents: (view_context.link_to video.mecha.full_name, mecha_path(video.mecha), class: 'no-decoration')}
     elsif video_param[:player_id].present?
-      video.update_attribute :player_id, video_param[:player_id]
+      update_attribute_of_continuous_videos(video, :player_id, video_param[:player_id])
       render json: {result: 'success', contents: (view_context.link_to video.player.name, player_path(video.player), class: 'no-decoration')}
     else
       render json: {result: 'failure'}
+    end
+  end
+
+  private
+
+  def update_attribute_of_continuous_videos(video, attr, value)
+    video.update_attribute(attr, value)
+    if video.win?
+      video = video.next_video
+      loop do
+        break if video.nil? or video[attr]
+        video.update_attribute(attr, value)
+        if video.lose?
+          break
+        else
+          video = video.next_video
+        end
+      end
     end
   end
 end
