de1dcdc52bcee69b580863d8ea7a84b0e10c11ca
Altech <takeno.sh@gmail.com>
2014-08-27 02:51:59 +0900

    add 16 images of pilots

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,8 +3,8 @@ class PlayersController < ApplicationController
   def index
     players = Player.all.sort_by(&:winning_percentage).reverse
     @players =
-      players.select{|player| player.videos.size > 18} +
-      players.select{|player| 7 < player.videos.size and player.videos.size <= 18} +
+      players.select{|player| player.videos.size > 17} +
+      players.select{|player| 7 < player.videos.size and player.videos.size <= 17} +
       players.select{|player| player.videos.size <= 7}
     @stats = Rails.cache.fetch(:stats_of_players, expires_in: 3.day) do
       Hash[players.map{|player| [player.id, calc_stat(player, :ratio)]}]
