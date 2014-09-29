e6b46647f7dbcf985f22443eacac14e1b5dc2509
Altech <takeno.sh@gmail.com>
2014-09-29 19:16:12 +0900

    change the function about player from CRUD to read only. closed #10

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -27,44 +27,6 @@ class PlayersController < ApplicationController
     render layout: false if request.headers['X-PJAX']
   end
 
-  def new
-    @player = Player.new
-  end
-
-  def create
-    player = Player.create(player_params)
-    if player.valid?
-      flash[:lets_login] = 'プレイヤー登録が完了しました。ログインしてください。'
-      redirect_to login_path
-    else
-      flash.now.alert = player.errors.full_messages.join("<br>")
-      @player = Player.new(player_params_without_password)
-      render action: 'new' and return
-    end
-  end
-
-  def edit
-    player = Player.find_by_name(params[:id])
-    if current_player and current_player == player
-      @player = player
-    end
-  end
-
-  def update
-    player = Player.find_by_name(params[:id])
-    if current_player and current_player == player
-      player.update_attributes(player_params_for_edit)
-      @player = Player.find(player.id)
-      error_messages = player.errors.full_messages_for(:name) + player.errors.full_messages_for(:mail)
-      if error_messages.size > 0
-        flash[:alert] = error_messages.join("<br>")
-      else
-        flash[:success] = "プレイヤー情報が更新されました"
-      end
-      redirect_to action: :edit, id: @player.to_param
-    end
-  end
-
   private
 
   def calc_stat(player, type = :absolute)
@@ -85,18 +47,4 @@ class PlayersController < ApplicationController
       stat.map{|name, size| [name, ((size/sum)*100).to_i]}
     end
   end
-
-  def player_params
-    params.require(:player).permit(:name,:password,:password_confirmation,:mail, :pilot_id)
-  end
-
-  def player_params_password
-    params.require(:player).permit(:password,:password_confirmation)
-  end
-
-  def player_params_without_password
-    params.require(:player).permit(:name,:mail,:pilot_id)
-  end
-
-  alias :player_params_for_edit :player_params_without_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ VsSeed::Application.routes.draw do
   resources :videos, only: %i[show update]
   resources :events, only: %i[index show edit update], constraints: {id: /\d\d\d\d-\d\d-\d\d/ }
   resources :mechas, only: %i[index show]
-  resources :players, only: %i[index show create new edit update]
+  resources :players, only: %i[index show]
 
   if Rails.env.production?
     match '*not_found' => 'application#render_404', via: [:get, :post]
